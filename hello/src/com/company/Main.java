68fae51126a29d5e739480ba83a88c2adf3d36fd
Serdar Yildiz <72278145+SerdarYildiz06@users.noreply.github.com>
2020-10-17 15:43:04 +0300

    Hello World
    I know this is very classic but it means a lot to me

---
+++ hello/src/com/company/Main.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        // Impossible is a word only to be found in the dictionary of fools.
+    }
+}
